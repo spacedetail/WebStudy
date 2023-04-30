b9f235eb233b067b67a65f80d2056944caa19826
HongKilDong <serbible9@gmail.com>
2023-04-30 13:06:36 +0900

    사용자 아바타 보여주기

--- blog/models.py
+++ blog/models.py
@@ -79,3 +79,9 @@ class Comment(models.Model):
 
     def get_absolute_url(self):
         return f'{self.post.get_absolute_url()}#comment-{self.pk}'
+
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://doitdjango.com/avatar/id/1450/4008dadc5d61792d/svg/{self.author.email}/'
--- blog/views.py
+++ blog/views.py
@@ -198,9 +198,9 @@ class PostSearch(PostList):
     def get_context_data(self, **kwargs):
         context = super(PostSearch, self).get_context_data()
         q = self.kwargs['q']
-        context['search_info'] = f'Search:{q} ({self.get_queryset().count()})'
-        return context
+        context['search_info'] = f'Search: {q} ({self.get_queryset().count()})'
 
+        return context
 
 
 
